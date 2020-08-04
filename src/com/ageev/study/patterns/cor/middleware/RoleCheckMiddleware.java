c8f3c2b24ffcd71dea68ffd55baa523ee749a009
Anatolii Ageev <anatol1812@gmail.com>
2020-08-04 20:04:33 +0300

    Added chain of responsibility pattern

---
+++ src/com/ageev/study/patterns/cor/middleware/RoleCheckMiddleware.java
@@ -0,0 +1,15 @@
+package com.ageev.study.patterns.cor.middleware;
+
+/**
+ * ConcreteHandler. Checks a user's role.
+ */
+public class RoleCheckMiddleware extends Middleware {
+    public boolean check(String email, String password) {
+        if (email.equals("admin@example.com")) {
+            System.out.println("Hello, admin!");
+            return true;
+        }
+        System.out.println("Hello, user!");
+        return checkNext(email, password);
+    }
+}
